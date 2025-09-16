411a17db7c1b27f25815fc54502dbd4d5253d37f
Adam Colyer <adamcolyer@gmail.com>
2025-09-16 16:03:03 -0500

    Add files via upload

---
+++ AssignementCodeWarsJ.py
@@ -0,0 +1,20 @@
+1.
+def even_or_odd(number):
+    return "Even" if number % 2 == 0 else "Odd"
+2.
+def number_to_string(num):
+    return str(num)
+3.
+def no_space(x):
+    return x.replace(" ", "")
+4. 
+def get_count(sentence):
+    vowels = "aeiou"
+    count = 0
+
+    for char in sentence:
+        if char in vowels:
+            count += 1
+
+    return count
+
